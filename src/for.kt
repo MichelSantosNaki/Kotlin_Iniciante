6e861851a22db77f8a71a55d914925db9fcb3e20
Michel dos Santos <michelsantos15@gmail.com>
2020-07-18 06:57:05 -0300

    for em kotlin

---
+++ src/for.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var nome: String = "Michel dos Santos"
+
+    // interando na variavel
+
+//    for(c in nome) println("$c")
+//    for(i in 1..100 step 2) {println("$i")} // interador de 1 a sem com passo 2
+
+    //for maior pro menor
+    for (i in 100 downTo 1 step 5) println(i)
+}
